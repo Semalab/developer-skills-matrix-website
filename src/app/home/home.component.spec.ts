16ecdcda497e2bba3a5886c1f5d5c33de192d5ba
Naomi Carrigan <nhcarrigan@gmail.com>
2022-06-30 09:31:29 -0700

    feat(tools): tests for home

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -5,19 +5,44 @@ import { HomeComponent } from './home.component';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let compiled: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-
+      declarations: [HomeComponent],
+    }).compileComponents();
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the title', () => {
+    const h1 = compiled.querySelector('h1');
+    expect(h1.textContent).toContain('Developer Skills Matrix');
+  });
+
+  it('should render the introduction text', () => {
+    const ps = compiled.querySelectorAll('p');
+    expect(ps.length).toBe(2);
+    expect(ps[0].textContent).toContain(
+      "Welcome to Sema's Developer Skills Matrix!"
+    );
+    expect(ps[1].textContent).toContain(
+      'This is a self-assessment tool designed to allow you to gauge where you are at in your development journey.'
+    );
+  });
+
+  it('should render the navigation buttons', () => {
+    const buttons = compiled.querySelectorAll('a');
+    expect(buttons.length).toBe(2);
+    expect(buttons[0].textContent).toContain('View the Matrix');
+    expect(buttons[1].textContent).toContain('Take the Self Assessment');
+    expect(buttons[0].getAttribute('routerLink')).toBe('/matrix');
+    expect(buttons[1].getAttribute('routerLink')).toBe('/quiz');
   });
 });
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,7 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {
   }
 
